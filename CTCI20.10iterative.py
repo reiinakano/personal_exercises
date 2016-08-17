291e84bbcb5ddce584d24583acae97a9713e4f59
reiinakano <reii_nakano@yahoo.com>
2016-08-18 04:07:54 +0800

    added CTCI 20.10 iterative version

---
+++ CTCI20.10iterative.py
@@ -0,0 +1,47 @@
+import enchant
+import string
+from collections import deque
+
+class WordNode:
+    def __init__(self, word, parent):
+        self.word = word
+        self.parent = parent
+
+
+def change_nth_index_in_string(word, index, character):
+    dummy = list(word)
+    dummy[index] = character
+    return "".join(dummy)
+
+
+def get_word_modifications(word):
+    for index, char in enumerate(word):
+        for letter in string.ascii_lowercase:
+            if letter != char:
+                yield change_nth_index_in_string(word, index, letter)
+
+
+def convert_word(original, target, dictionary, words_used={}):
+    q = deque([WordNode(original, None)])
+    words_used[original] = True
+    while q:
+        current = q.popleft()
+        if current.word == target:
+            to_return = ""
+            while True:
+                to_return += " " + current.word
+                current = current.parent
+                if not current:
+                    break
+            to_return = " ".join(reversed(to_return.split(" ")))
+            return to_return  # Change this obviously
+        for word in get_word_modifications(current.word):
+            if dictionary.check(word) and word not in words_used:
+                q.append(WordNode(word, current))
+                words_used[word] = True
+    return "nay"
+
+
+if __name__ == "__main__":
+    d = enchant.Dict("en_US")
+    print convert_word("steam", "queen", d)
